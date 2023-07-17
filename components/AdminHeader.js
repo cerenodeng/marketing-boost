19b40bba7228ed427f19031879d133d1e249004a
Cereno Deng <cereno.deng@outlook.com>
2023-07-18 03:35:38 +0800

    improve admin header styles

--- components/AdminHeader.js
+++ components/AdminHeader.js
@@ -1,18 +1,15 @@
-import Link from 'next/link';
-
 export default function AdminHeader({ title }) {
-
   return (
-    <div className="flex justify-between">
+    <div className="flex gap-x-5 items-center">
       <h1 className="text-3xl font-bold">{title}</h1>
 
-      <div className="flex gap-x-5">
-        <Link href="#" className="flex text-white bg-indigo-900 px-5 py-2.5 items-center border-2 border-transparent rounded hover:text-indigo-900 hover:bg-white hover:border-2 hover:border-indigo-900">
+      <form className="flex gap-x-5">
+        <button className="flex text-white bg-indigo-900 p-2.5 border-2 border-transparent rounded hover:text-indigo-900 hover:bg-white hover:border-2 hover:border-indigo-900">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
           </svg>
-        </Link>
-      </div>
+        </button>
+      </form>
     </div >
   )
 }
