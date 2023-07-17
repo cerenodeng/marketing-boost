27ea276ff08f6e34dc6e9eb42689f3efaa19e135
Cereno Deng <cereno.deng@outlook.com>
2023-07-18 03:00:35 +0800

    add admin list

--- app/admin/quizzes/page.js
+++ app/admin/quizzes/page.js
@@ -1,11 +1,31 @@
 import AdminHeader from '@/components/AdminHeader';
+import AdminList from '@/components/AdminList';
+
+async function refreshQuizzes(term) {
+  'use server';
+  const data = [];
+
+  return data;
+}
+
+async function deleteQuiz(id) {
+  'use server';
+  const data = [];
+
+  return data;
+}
 
 export default function Quizzes() {
+  const originalItems = [
+    { id: 1, title: 'Quizz One', start_date: '2023-7-18', start_time: '9:00', end_date: '2023-7-19', end_time: '12:00' },
+    { id: 2, title: 'Quizz Two', start_date: '2023-8-18', start_time: '10:00', end_date: '2023-8-19', end_time: '13:00' },
+    { id: 3, title: 'Quizz Three', start_date: '2023-9-18', start_time: '11:00', end_date: '2023-9-19', end_time: '14:00' },
+  ]
 
   return (
     <main className="flex flex-col gap-y-10 p-8 w-full">
       <AdminHeader title="Quizzes" />
-      quizzes list
+      <AdminList originalItems={originalItems} refreshItems={refreshQuizzes} deleteItem={deleteQuiz} />
     </main>
   );
 }
