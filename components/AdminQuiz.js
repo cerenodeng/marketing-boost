a4c652a814098301af292ea2ca3fb5026a43880e
Cereno Deng <cereno.deng@outlook.com>
2023-08-14 23:58:22 +0800

    change admin quiz adding layout

--- components/AdminQuiz.js
+++ components/AdminQuiz.js
@@ -1,19 +1,30 @@
 export default function AdminQuiz() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="p-12">
-        <div>
-          <input id="single" name="type" type="radio" value="1" />
-          <label htmlFor="single">Single Choice</label>
-          <input id="multiple" name="type" type="radio" value="1" />
-          <label htmlFor="multiple">Multiple Choice</label>
+    <div className="flex gap-x-10">
+      <div className="flex flex-col gap-y-5 w-1/3">
+        <div className="bg-gray-100 p-2.5 rounded">
+          <div className="flex gap-x-5">
+            <div className="flex gap-x-1 items-center">
+              <input id="single" className="w-5 h-5" name="type" type="radio" value="1" />
+              <label htmlFor="single">Single Choice</label>
+            </div>
+            <div className="flex gap-x-1 items-center">
+              <input id="multiple" className="w-5 h-5" name="type" type="radio" value="2" />
+              <label htmlFor="multiple">Multiple Choice</label>
+            </div>
+          </div>
         </div>
+
         <button className="action">
           <svg className="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
           </svg>
           Add
         </button>
+      </div>
+
+      <div className="flex flex-col gap-y-5 w-2/3">
+        Preview
       </div>
     </div>
   )
