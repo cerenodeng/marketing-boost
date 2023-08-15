e75fda4ab491306f57fe4df8e66583ad2b451440
Cereno Deng <cereno.deng@outlook.com>
2023-08-16 00:00:01 +0800

    change admin quiz adding layout

--- components/AdminQuiz.js
+++ components/AdminQuiz.js
@@ -1,18 +1,38 @@
+import { useState } from 'react';
+
 export default function AdminQuiz() {
+  const [type, setType] = useState('single');
+
+  function handleType(event) {
+    setType(event.target.value);
+  }
+
   return (
     <div className="flex gap-x-10">
       <div className="flex flex-col gap-y-5 w-1/3">
         <div className="bg-gray-100 p-2.5 rounded">
           <div className="flex gap-x-5">
             <div className="flex gap-x-1 items-center">
-              <input id="single" className="w-5 h-5" name="type" type="radio" value="1" />
+              <input id="single" className="w-5 h-5" name="type" type="radio" value="single" checked={type == 'single'} onChange={handleType} />
               <label htmlFor="single">Single Choice</label>
             </div>
             <div className="flex gap-x-1 items-center">
-              <input id="multiple" className="w-5 h-5" name="type" type="radio" value="2" />
+              <input id="multiple" className="w-5 h-5" name="type" type="radio" value="multiple" checked={type == 'multiple'} onChange={handleType} />
               <label htmlFor="multiple">Multiple Choice</label>
             </div>
           </div>
+
+          {type == 'single' &&
+            <div className="">
+              Questions for single
+            </div>
+          }
+
+          {type == 'multiple' &&
+            <div className="">
+              Questions for multiple
+            </div>
+          }
         </div>
 
         <button className="action">
