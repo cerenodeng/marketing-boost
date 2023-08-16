214e2ddde50f2731a0d13facc2e7e94cc9bd4f10
Cereno Deng <cereno.deng@outlook.com>
2023-08-17 00:57:53 +0800

    add label input

---
+++ components/LabelInput.js
@@ -0,0 +1,19 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function LabelInput({ label, name, placeholder }) {
+  const [emptyInput, setEmptyInput] = useState(true);
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <label className={emptyInput ? 'invisible' : 'visible'} htmlFor={name}>{label}</label>
+      <input
+        className="placeholder:italic"
+        name={name}
+        placeholder={placeholder}
+        onChange={(event) => event.target.value.length == 0 ? setEmptyInput(true) : setEmptyInput(false)}
+      />
+    </div>
+  )
+}
